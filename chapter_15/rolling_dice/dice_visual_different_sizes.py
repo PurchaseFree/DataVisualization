2896e9eefad50e1e895145ee21ca5b145565794a
richadoy <richado.y@outlook.com>
2022-05-24 21:59:12 +0800

    add some annotation

--- chapter_15/rolling_dice/dice_visual_different_sizes.py
+++ chapter_15/rolling_dice/dice_visual_different_sizes.py
@@ -3,29 +3,29 @@ from plotly import offline
 
 from die import Die
 
-# Create a D6 and a D10.
+# 创建一个D6和D10的骰子
 die_1 = Die()
 die_2 = Die(10)
 
-# Make some rolls, and store results in a list.
+# 多次投掷骰子，将结果存储在results列表中
 results = []
 for roll_num in range(50_000):
     result = die_1.roll() + die_2.roll()
     results.append(result)
-    
-# Analyze the results.
+
+# 分析结果
 frequencies = []
 max_result = die_1.num_sides + die_2.num_sides
-for value in range(2, max_result+1):
+for value in range(2, max_result + 1):
     frequency = results.count(value)
     frequencies.append(frequency)
-    
-# Visualize the results.
-x_values = list(range(2, max_result+1))
+
+# 可视化的结果
+x_values = list(range(2, max_result + 1))
 data = [Bar(x=x_values, y=frequencies)]
 
 x_axis_config = {'title': 'Result', 'dtick': 1}
 y_axis_config = {'title': 'Frequency of Result'}
 my_layout = Layout(title='Results of rolling a D6 and a D10 50000 times',
-        xaxis=x_axis_config, yaxis=y_axis_config)
+                   xaxis=x_axis_config, yaxis=y_axis_config)
 offline.plot({'data': data, 'layout': my_layout}, filename='d6_d10.html')
